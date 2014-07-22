76eb2b3b6c880b7a1afb0b67582b7735599cdf4e
Clint Kelly <clint@wibidata.com>
2014-07-22 16:45:39 -0700

    Edit to use build helper plugin for port assignment.

--- src/test/java/org/kiji/fiddle/AppIT.java
+++ src/test/java/org/kiji/fiddle/AppIT.java
@@ -3,6 +3,9 @@ package org.kiji.fiddle;
 
 import static org.junit.Assert.assertFalse;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
 import org.junit.Test;
@@ -16,12 +19,30 @@ public class AppIT {
   private static final Logger LOG = LoggerFactory.getLogger(AppIT.class);
   @Test
   public void simpleTest() {
-    Cluster cluster = Cluster.builder()
-        .addContactPoint("127.0.0.10")
-        .build();
-    Session session = cluster.connect();
-    LOG.info("Opened connection to cluster!");
-    session.close();
-    cluster.close();
+    // Load properties file from classpath.
+    Properties prop = new Properties();
+    InputStream input = null;
+
+    try {
+      input = this.getClass().getClassLoader().getResourceAsStream("cassandra-maven-plugin.properties");
+
+      // load a properties file
+      prop.load(input);
+
+      input.close();
+
+      Cluster cluster = Cluster.builder()
+          .addContactPoint(prop.getProperty("cassandra.initialIp"))
+          .withPort(Integer.parseInt(prop.getProperty("cassandra.nativePort")))
+          .build();
+      Session session = cluster.connect();
+      LOG.info("Opened connection to cluster!");
+      System.out.println("Running test");
+      session.close();
+      cluster.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+      org.junit.Assert.assertFalse("Failed if we got here", true);
+    }
   }
 }
