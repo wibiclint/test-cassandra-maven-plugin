9fba951ea2e5c6f1a7bafdf3ed75bd932c4e3532
Clint Kelly <clint@wibidata.com>
2014-07-22 11:26:16 -0700

    First commit.

---
+++ src/test/java/org/kiji/fiddle/AppIT.java
@@ -0,0 +1,27 @@
+package org.kiji.fiddle;
+
+
+import static org.junit.Assert.assertFalse;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppIT {
+  private static final Logger LOG = LoggerFactory.getLogger(AppIT.class);
+  @Test
+  public void simpleTest() {
+    Cluster cluster = Cluster.builder()
+        .addContactPoint("127.0.0.10")
+        .build();
+    Session session = cluster.connect();
+    LOG.info("Opened connection to cluster!");
+    session.close();
+    cluster.close();
+  }
+}
